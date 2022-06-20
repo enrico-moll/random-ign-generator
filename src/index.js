4b37ddc5333aaa501a6fdd6a346d53608a27afea
Enrico Moll <enrico.jordanmoll@gmail.com>
2022-06-20 14:43:11 +0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const RandomIGNGenerator = () => {
+  const arr1 = ["red", "green", "blue", "yellow"];
+  const arr2 = ["test", "random", "sample"];
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  const ign1 = arr1[Math.floor(Math.random() * arr1.length)];
+  const ign2 = arr2[Math.floor(Math.random() * arr2.length)];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return ign1 + ign2;
+};
+
+export default RandomIGNGenerator;
